979d2d9b04cc93912521250418ced439a6b3e5ee
AhiShahar <ahishahar1987@gmail.com>
2016-10-28 16:20:41 +1100

    updates from Badger

--- ahi_shahar/week_01/main_1_4.js
+++ ahi_shahar/week_01/main_1_4.js
@@ -47,7 +47,7 @@ var triangle = {
     var area = (number*(number-this.sideA)*(number-this.sideB)*(number-this.sideC))/2;
     return area;
   },
-  isObtuse : function() {
+  isObtuse : function() { //Therefore, an obtuse triangle satisfies one of a^2+b^2<c^2, b^2+c^2<a^2, or c^2+a^2<b^2.
     num1 = this.sideA ^ 2;
     num2 = this.sideB ^ 2;
     num3 = this.sideC ^ 2;
@@ -161,3 +161,24 @@ var validateCreditCard = function (card, expDate) {
   if ( total < 16 ) return badData(card,false,"total_of_digits_under_16!",ccDate);
 return data(card,true,ccDate)
 };
+
+// JavaScript Bank
+// In this homework, you'll create a basic bank in Javascript. The bank has many accounts and the following capabilities that you need to write.
+// Bank
+// There is only one bank. This bank has an array of accounts. The bank needs a method that will return the total sum of money in the accounts. It also needs an addAccount method that will enroll a new account at the bank and add it to the array of accounts. There is no need to create additional functions of the bank to delete accounts, etc.
+// The bank has many accounts. Accounts should be objects that all share a set of common functionality.
+var bank = [];
+var newAcc = {
+  
+}
+// Accounts
+// Accounts have a current balance and owner's name. You should be able to deposit or withdraw from an account to change the balance.
+// There is no need to write a user interface. Make sure functions return values -- you may also have your functions console.log() values to help you see your code working
+// You should write a basic story through a series of JavaScript commands that shows that the methods do indeed work as expected: add some accounts, show the total balance, make some deposits and withdrawals, show the new total balance.
+// Tips
+// Don't overthink this. Shorter code is probably the answer.
+// Bonus
+// Ensure that the accounts cannot have negative values.
+// Write a 'transfer' on the bank that allows you to transfer amounts between two accounts.
+// Additional
+// Begin exploring the JavaScript Koans. Fork, clone and start trying them.
