102b0fffdc463115a95c867972382b6fb18fe6b4
AhiShahar <ahishahar1987@gmail.com>
2016-10-29 19:20:37 +1100

    all going good but had a few beers, saving just in case

--- ahi_shahar/week_01/MTA/main.js
+++ ahi_shahar/week_01/MTA/main.js
@@ -31,3 +31,92 @@
 // Consider diagramming the lines by sketching out the subway lines and their stops and intersection.
 // The key to the lab is finding the index positions of each stop. (hint: indexOf())
 // Make sure the stops that are the same for different lines have different names (i.e. 23rd on the N and on the 6 need to be differentiated)
+var reverseArr = function (myArray) {
+  for (i = 0; i < myArray.length / 2 ; i++) { // '/2' to stop the function from going back and reversing itself
+    var old = myArray[i]; // store the original data from location 'i'
+    myArray[i] = myArray[myArray.length - 1 - i]; // store the data from the opposite side of the array into location 'i'
+    myArray[myArray.length - 1 - i] = old; // put back the old data from location 'i' into the opposite side of the array
+  };
+  return myArray;
+};
+var nLine = {
+  line : "N",
+  stops : ["Times Square", "34th", "28th", "23rd North", "Union Square", "8th North"]
+};
+var lLine = {
+  line : "L",
+  stops : ["8th", "6th", "Union Square", "3rd", "1st"]
+};
+var sLine = {
+  line : "6",
+  stops : ["Grand Central", "33rd", "28th St", "23rd", "Union Square", "Astor Place"]
+};
+var checkChange = function (trip,tripEnd) {
+  for (i = 0 ; i < trip.length ; i += 1) {
+    for ( x = 0 ; x < tripEnd.length ; x += 1) {
+      if (trip[i] === tripEnd[x]) {
+        return trip[i];
+      };
+    };
+  };
+};
+// pick the right lane to start the journey on
+var pickLine1 = function () {
+  if ( nLine.line === startLn) var trip = nLine.stops;
+  else if ( lLine.line === startLn) var trip = lLine.stops;
+  else if ( sLine.line === startLn) var trip = sLine.stops;
+  else return(console.log("You're a dick."));
+  return trip;
+};
+// pick the right lane to finish the journey on
+var pickLane2 = function (end.Ln) {
+  if ( nLine.line === endLn) var tripEnd = nLine.stops;
+  else if ( lLine.line === endLn) var tripEnd = lLine.stops;
+  else if ( sLine.line === endLn) var tripEnd = sLine.stops;
+  else console.log( "You're a dick.");
+};
+var planTrip = function (startPt, startLn, endLn, endPt) {
+  startLn = startLn.toUpperCase().charAt(0);
+  endLn = endLn.toUpperCase().charAt(0);
+  if (startLn.valueOf() === endLn.valueOf()) { // check if the user is a cunt
+    if (startPt.valueOf() === endPt.valueOf()) { // meaning, is he an idiot and is already at his destination
+      return prompt("You're already at your destination you fool.")
+    };
+  };
+  // start line
+  var trip = pickLine1( startLn );
+  if (startLn.valueOf() === endLn.valueOf()) { // check if the user needs to change lines if he doesn't, lets sum things up
+    trip.pop();
+    trip.shift();
+    var bonVoyage = "You are traveling from " + startPt + " on line " + startLn + ".\n" +
+    "Travel through " + trip.join(", ") + " all the way to " + endPt + ".\n" + (trip.length + 1) + " stops in total.";
+    alert(bonVoyage);
+    return;
+  };
+  // end line
+  var tripEnd = pickLine2( endLn );
+  // check connections
+  var changePoint = checkChange(trip,tripEnd);
+  // The next 2 conditions check the direction of travel and return the array facing the right way
+  if ( tripEnd.indexOf(endPt) < tripEnd.indexOf(changePoint)) {
+    tripEnd = reverseArr(tripEnd);
+  };
+  if (trip.indexOf(startPt) > trip.indexOf(changePoint)) {
+    trip = reverseArr(trip);
+  };
+  trip = trip.slice( trip.indexOf( startPt ) , trip.indexOf( changePoint ) );
+  tripEnd =  tripEnd.slice( tripEnd.indexOf( changePoint ) +1 ,tripEnd.indexOf( endPt ) );
+  var totalStops = trip.length + tripEnd.length + 1;
+  // var finalTrip = trip.concat(tripEnd); // not completely neccesary ATM as its just as easy to count 2 arrays, but for future use (if you'll want to connect two lines and find where they would meet another line)
+  trip.shift();
+  var bonVoyage = "You are traveling from " + startPt + " on line " + startLn + " going through " + trip.join(", ") + ",\n" +
+   "Change lines at " + changePoint + ".\n" +
+    "Take line " + endLn + " and continue through " + tripEnd.join(", ") + " all the way to " + endPt + ".\n" + totalStops + " stops in total.";
+  console.log(bonVoyage);
+};
+planTrip(
+  startPt = prompt("Where are you starting your trip?"),
+  startLn = prompt("on which line?"),
+  endLn = prompt("any line you would like to change to?"),
+  endPt = prompt("and where are you going to?")
+);
