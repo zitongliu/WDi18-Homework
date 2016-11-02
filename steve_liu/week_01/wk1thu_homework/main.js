f982616cf03cc62d1e88d4f8c9a1c0f61cb79693
zitongliu <zitongliu7@gmail.com>
2016-11-02 21:50:52 +1100

    reuploaded wk1 Thursday homework

--- steve_liu/week_01/wk1thu_homework/main.js
+++ steve_liu/week_01/wk1thu_homework/main.js
@@ -61,7 +61,7 @@ var triangle = {
   sideB: 3,
   sideC: 4,
   isEquilateral:function(){
-    result;
+    var result;
     if (this.sideA === this.sideB && this.sideB === this.sideC){
       console.log("The triangle is equilateral");
       result = true;
@@ -79,7 +79,7 @@ var triangle = {
       console.log("The triangle is Isosceles");
     } else{
       console.log("The triangle is not Isosceles");
-    };
+    }
   },
   area: function(){
     var s = (this.sideA + this.sideB + this.sideC) / 2;
@@ -135,11 +135,11 @@ var cartForParty = {
 
 var cashRegister = function(inputObject){
   var totalCost = 0;
-  for (item in inputObject){
+  for (var item in inputObject){
     totalCost = totalCost + Number(  inputObject[item]  );
-  };
+  }
   console.log("The total cost is:",totalCost);
-}
+};
 
 cashRegister(cartForParty);
 
@@ -181,6 +181,149 @@ and what are some numbers that pass or fail? Ideas: check expiration date!
 Check out the Luhn Algorithm for inspiration.
 */
 
-var validCreditCard = function (){
-  
+var validCreditCard = function (inputNumber){
+  var isValid = true;                 // by default, isVaid = true
+  var creditNumber = inputNumber;     // string
+
+  //    check if the length of the input string is 19 (16 numbers, 3 dashes)
+  if ( creditNumber.length !== 19 ){
+    isValid = false;
+    console.log("Numbers must be 16 digits");
+  }
+
+  //    check if numbers are in groups of 4 digits seperated by dash
+  if ( creditNumber[4] !== "-" || creditNumber[9] !== "-" || creditNumber[14] !== '-' ){
+    isValid = false;
+    console.log("Numbers must be groups of 4");
+  }
+
+  //    check if last digit is odd
+  if ( creditNumber.slice(-1) % 2 !==0 ) {
+    isValid = false;
+    console.log("The last digit must be even");
+  }
+
+  //  to check sum, we remove dash from input string and then convert it to array then to number then add
+  var creditNumberNoDash = creditNumber.replace(/-/g,''); // regular expression. g removes all instances in string
+  var creditNumberArray = creditNumberNoDash.split("").map(Number); // .map(Number) coverts to number
+  var sumCreditNumberNoDash = 0;
+  for (i = 0 ; i < creditNumberArray.length ; i+=1){
+    sumCreditNumberNoDash = sumCreditNumberNoDash + creditNumberArray[i];
+  }
+  if ( sumCreditNumberNoDash <= 16 ){
+    isValid = false;
+    console.log( "The sum of digits must be more than 16" );
+  }
+  // Check if the credit card number contains characters. If it contains only numbers, the below
+  // regular expression should return true
+  onlyNumbers = /^\d+$/.test(creditNumberNoDash);
+  if ( onlyNumbers === false ){
+    isValid = false;
+    console.log("The input must only contain numbers");
+  }
+
+
+  return isValid;
+};
+validCreditCard('1111-2222-3233-4444');
+
+
+/*
+JavaScript Bank
+
+In this homework, you'll create a basic bank in Javascript. The bank has many
+accounts and the following capabilities that you need to write.
+
+Bank
+
+There is only one bank. This bank has an array of accounts.
+The bank needs a method that will return the total sum of money in the accounts.
+It also needs an addAccount method that will enroll a new account at the bank
+and add it to the array of accounts. There is no need to create additional functions
+of the bank to delete accounts, etc.
+
+The bank has many accounts. Accounts should be objects that all share a set of common functionality.
+
+Accounts
+
+Accounts have a current balance and owner's name. You should be able to deposit
+or withdraw from an account to change the balance.
+
+There is no need to write a user interface. Make sure functions return values
+-- you may also have your functions console.log() values to help you see your code working.
+
+You should write a basic story through a series of JavaScript commands that shows
+that the methods do indeed work as expected: add some accounts, show the total balance,
+make some deposits and withdrawals, show the new total balance.
+
+Tips
+
+Don't overthink this. Shorter code is probably the answer.
+
+Bonus
+
+Ensure that the accounts cannot have negative values.
+Write a 'transfer' on the bank that allows you to transfer amounts between two accounts.
+*/
+
+
+
+/*
+//  Constructor pattern to create object
+var bankAccounts = [];
+var Account = function(inputFirstName,inputLastName){
+  this.currentBalance = 0;  // by default balance is zero
+  this.firstName = inputFirstName;
+  this.lastLast = inputLastName;
+  this.deposit = function (amountIn){
+    this.currentBalance += amountIn;
+    console.log("Deposited:",amountIn);
+    return this.currentBalance;
+  };
+  this.withdraw = function(amountOut){
+    this.currentBalance -= amountOut;
+    console.log("Withdrawn:",amountOut);
+    return this.currentBalance;
+  }
+  bankAccounts.push(this);
+  return this;
+};
+
+var steveAccount = new Account("Steve","Liu")
+*/
+
+var bankAccounts = [];
+var totalAmountStoredInBank = 0;
+
+// Factory pattern to make object
+var AccountFactory = function( inputFirst, inputLast ){
+  accountObject = {
+    firstName:inputFirst,
+    lastName:inputLast,
+    currentBalance:0,
+    deposit:function( amountIn ){
+      this.currentBalance += amountIn;
+      console.log( "Deposited:", amountIn );
+      return this.currentBalance;
+    },
+    withdraw:function( amountOut ){
+      if ( this.currentBalance  <  amountOut ){  // check if withdraw amount is more than balance
+        console.log( "Can not withdraw, balance not enough!" );
+        return; // break out of the function
+      }
+      this.currentBalance -= amountOut;
+      console.log( "Withdrawn:", amountOut );
+      return this.currentBalance;
+    },
+  };
+  bankAccounts.push( accountObject ); // add the new object to the acount array
+
+  return accountObject;
+};
+
+// a for loop to sum the balance in every account
+var totalAmountStoredInBank = 0;
+for ( i = 0 ; i < bankAccounts.length ; i += 1){
+  totalAmountStoredInBank += bankAccounts[i]["currentBalance"];
 }
+console.log( "The total money stored in this banks is:", totalAmountStoredInBank );
