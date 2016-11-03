d7d8bad9f83300b20a198b0f10490eab150e9e6d
emmafallancy <emmanuel.fallancy@gmail.com>
2016-11-03 12:50:46 +1100

    Made small adjustment to the animation js

--- emmanuel_fallancy/week_02/day_3/main.js
+++ emmanuel_fallancy/week_02/day_3/main.js
@@ -1,15 +1,16 @@
 console.log( "Annoying cat" );
 
-var box = document.querySelector( "div" );
+var box = document.querySelector( "img" );
 
 box.style.left = "0";
 box.style.right = "0";
+box.style.width = "100px";
 
 console.log( box.style.left, box.style.right );
 
 var moveForward = function () {
    var leftPos = parseInt( box.style.left );
-   leftPos += 100;
+   leftPos += 40;
    box.style.left = leftPos + "px";
 
    return leftPos;
@@ -17,30 +18,41 @@ var moveForward = function () {
 
 var moveBackward = function () {
   var leftPos = parseInt( box.style.left );
-  leftPos -= 100;
+  leftPos -= 40;
   box.style.left = leftPos + "px";
 
   return leftPos;
 }
 
 var isMovingForward = true;
+var movingAcross = 0;
 
 var moveAcross = function () {
   var leftPos = parseInt( box.style.left );
 
-  if ( leftPos < window.screen.width && isMovingForward ) {
-      leftPos = moveForward();
-      if ( leftPos > window.screen.width ) {
-        isMovingForward = false;
-      }
+  if ( movingAcross > 10 ) {
+    box.style.display = "none";
+    console.log( "You did the magic!" );
+    window.clearInterval( movingCat );
   }
-  else if ( leftPos > window.screen.availLeft && !isMovingForward ) {
 
-      leftPos = moveBackward();
-      if ( leftPos <= window.screen.availLeft ) {
-        isMovingForward = true;
+  if ( leftPos < ( window.innerWidth - parseInt( box.style.width ) ) && isMovingForward ) {
+      leftPos = moveForward();
+      if ( leftPos > window.innerWidth - parseInt( box.style.width ) ) {
+        box.style.transform = "scale( -1, 1 )";
+        isMovingForward = false;
+        movingAcross += 1;
       }
   }
-  
+  else if ( leftPos > 0 && !isMovingForward ) {
+      leftPos = moveBackward();
+      if ( leftPos <= 0 ) {
+        box.style.transform = "scale( 1, 1 )";
+        isMovingForward = true;
+        movingAcross += 1;
+      }
+  }
+
 };
-window.setInterval( moveAcross, 50 );
+
+var movingCat = window.setInterval( moveAcross, 100 );
